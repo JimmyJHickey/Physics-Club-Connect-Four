ee0e5c25ba711adcc4e89ea203bd77c55fdfa925
DuPont <JD048966@cerner.net>
2017-11-08 10:57:44 -0600

    Added comments

--- Connect4.ino
+++ Connect4.ino
@@ -11,28 +11,31 @@
 
 #define MATRIX_PIN 6
 
+// Convenient way to hold data 
 struct Point{
   byte x;
   byte y;
 };
 
-
+// Init the matrix with size and shape arguments
 Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(7, 6, MATRIX_PIN,
   NEO_MATRIX_BOTTOM  + NEO_MATRIX_LEFT +
   NEO_MATRIX_COLUMNS + NEO_MATRIX_PROGRESSIVE,
   NEO_GRB         + NEO_KHZ800);
 
 
-void setup() {
-  matrix.begin();
-  matrix.setBrightness(16);
 
-	Serial.begin(9600);
+void setup() {
+  matrix.begin();	// Get the matrix going
+  matrix.setBrightness(16);	// Make it not blinding
+
+  Serial.begin(9600);	// Debug and current data entry method
   redraw();
 }
+
 int incomingByte = 0;   // for incoming serial data
-byte turn = 0;
-byte board[7][6];
+byte turn = 0;	// Who's turn it is, either 0 or 1
+byte board[7][6];	// Init the board (top left of board is 0, 0)
 boolean showed = false;
 void loop() {
   if(!showed){
@@ -40,10 +43,10 @@ void loop() {
   }
   
   if (Serial.available() > 0) {
-    // read the incoming byte:
+    // read the typed byte
     incomingByte = Serial.read();
 
-    // say what you got:
+    // Byte should be between 1 and 7 (characters)
     if(incomingByte >= '1' && incomingByte <= '7'){
       if(runTurn(turn, incomingByte - '1')){
          turn = turn == 1 ? 0 : 1;
@@ -54,30 +57,34 @@ void loop() {
 }
 
 boolean runTurn(byte turn, byte pos){
-  if(board[pos][0]){
+  if(board[pos][0]){	// If the stack was full, return false
     return false;
   }
+  
   byte coord = 0;
-  for(int i = 0; i < 6; i++){
-    if (i != 0){
+  // Loop from top to bottom of the board, this is to animate the token falling
+  for(int i = 0; i < 6; i++){ 
+    if (i != 0){	// If this is not the top of the board, clear the previous slot
       board[pos][i-1] = 0;
     }
-    board[pos][i] = turn + 1;
-    if(board[pos][i+1] > 0 || i == 5){
+    board[pos][i] = turn + 1; // Set the current position to the correct players token (0 is empty, 1 is p1, 2 is p2)
+    if(board[pos][i+1] > 0 || i == 5){	// If the token hits the top of the stack or the bottom of the board, stop
       coord = i;
-      //break;
-      i = 10;
+      //break; // breaking ironicially breaks the arduino, don't use it
+      i = 10;	// This is sort of a break
     }
     redraw();
     delay(50);
   }
-  checkWin(turn + 1, (Point){pos, coord});
+  checkWin(turn + 1, (Point){pos, coord});	// If the new token causes a win then this function resets the board
   return true;
 }
 
 void checkWin(byte player, Point pos){
   LinkedList<Point> winList;
   LinkedList<Point> tempWinList;
+  
+  // Check for horizontal wins
   byte counter = 0;
   for(int i = 0; i < 8; i++){
     if(board[i][pos.y] == player && i < 7){
@@ -95,6 +102,7 @@ void checkWin(byte player, Point pos){
     }
   }
   
+  // Check for vertical wins
   counter = 0;
   for(int i = 0; i < 7; i++){
     if(board[pos.x][i] == player && i < 6){
@@ -112,6 +120,7 @@ void checkWin(byte player, Point pos){
     }
   }
 
+  // Check for -1 slope wins
   counter = 0;
   Point startPos = {pos.x - min(pos.x, pos.y), pos.y - min(pos.x, pos.y)};
   for(int i = 0; i < 7; i++){
@@ -130,6 +139,7 @@ void checkWin(byte player, Point pos){
     }
   }
 
+  // Check for +1 slope wins
   counter = 0;
   startPos = {pos.x - min(pos.x, 7 - pos.y), pos.y + min(pos.x, 7 - pos.y)};
   for(int i = 0; i < 7; i++){
@@ -148,6 +158,7 @@ void checkWin(byte player, Point pos){
     }
   }
 
+  // If the player wins, display win and reset the board
   if(winList.size() > 0){
     for(int i = 0; i < 10; i++){
       for(int j = 0; j < winList.size(); j++){
@@ -171,6 +182,7 @@ void checkWin(byte player, Point pos){
   }
 }
 
+// Converts the byte board into an image and draws it
 void redraw(){
   int16_t tempColor = matrix.Color(0, 0, 0);
   matrix.fillScreen(tempColor);
